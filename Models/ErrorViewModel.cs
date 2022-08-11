571bb0a3626d9613c3ed756951e9d8c9f67270ff
Anthony <ta4naya@yahoo.com>
2022-08-11 05:37:06 +0200

    first commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace kwadowebsite.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
